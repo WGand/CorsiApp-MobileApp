30699b651d557ba6c0b83fdae608b571f6c62371
Aitor <aitorparrilla06@gmail.com>
2022-12-04 00:29:23 -0400

    fix fake api

--- corsiapp/lib/Interf/https_fetcher.dart
+++ corsiapp/lib/Interf/https_fetcher.dart
@@ -3,7 +3,7 @@ import 'package:corsiapp/Application/json_decoder.dart';
 
 Future<JsonDecoder> fetchCourse() async {
   final response = await http
-      .get(Uri.parse('https://638c1e60eafd555746a0b852.mockapi.io/get'));
+      .get(Uri.parse('https://638c1e60eafd555746a0b852.mockapi.io/Course'));
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
